10526497f66022570ad61276fc56fcad4125a4a3
wangkj <>
2026-02-24 14:46:02 +0800

    add examples

---
+++ examples/demo.js
@@ -0,0 +1,20 @@
+const {
+  getSectorRank,
+  getRealTimeNet
+
+} = require('../lib');
+
+
+
+// 使用示例
+// (async () => {
+//   const data = await getSectorRank();
+//   console.log('板块排行:', data);
+// })();
+
+
+// 使用示例
+(async () => {
+  const data = await getRealTimeNet('002050');
+  console.log('实时净流入:', data);
+})();
