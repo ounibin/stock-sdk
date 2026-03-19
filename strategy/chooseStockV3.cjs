ce5b7b578628595993b5e0d68fef053a443901cd
wangkj <>
2026-03-19 13:20:25 +0800

    add v3

---
+++ strategy/chooseStockV3.cjs
@@ -0,0 +1,96 @@
+const dayjs = require('dayjs')
+const path = require('path')
+const fs = require('fs')
+const {
+  getAllQuotes,
+  getNetRealTime,
+  filterRedCrossStar,
+  getPreviousTradingDay,
+  getAllQuotesRealTime
+} = require('../lib')
+
+
+async function main(day1, useLocalData = false) {
+  const now = dayjs()
+  const todayStr = now.format('YYYYMMDD')
+
+
+
+  if (!day1) {
+    console.log('请提供有效的日期参数')
+    throw new Error('Invalid date parameter, please provide a valid date string in the format YYYYMMDD.')
+  }
+  const lastDay = await getPreviousTradingDay(day1)
+  const lastList = await getAllQuotes(lastDay)
+  try {
+    let list = []
+    if (useLocalData) {
+      console.log('使用本地数据，跳过网络请求')
+      list = await require(`./data/${day1}.json`)
+    } else {
+      console.log(`正在获取${day1}实时行情数据...`)
+      list = await getAllQuotesRealTime(day1)
+      const filePath = path.join(__dirname, `data/${day1}.json`)
+      fs.writeFileSync(filePath, JSON.stringify(list, null, 2))
+    }
+    console.log('上个开盘日: ', lastDay, '数据长度为', lastList.length)
+
+    if (day1.length === 0) {
+      console.log(`${day1}数据异常`)
+      return
+    } else if (lastDay.length === 0) {
+      console.log(`${lastDay}数据异常`)
+      return
+    }
+
+    // 条件选股
+    list = list.filter((item) => {
+      const isKechuang = /^688|689|787|789/.test(item.code)
+      const isXinSanBan = /^82|83|87|88|430|420|400/.test(item.code)
+      const isJingA = /^9/.test(item.code)
+      const bigPrice = item.trade > 10
+      const noTooMuchUp = item.changePercent < 7
+      return !isKechuang && !isXinSanBan && !isJingA && noTooMuchUp
+    })
+
+    const list1 = []
+    list.forEach((item) => {
+      const item_last = lastList.find((n) => n.code === item.code)
+      if (item_last && item.volume > item_last.volume * 1.5 && item_last.open > item_last.close && item.trade === item.high) {
+        list1.push(item)
+      }
+    })
+
+    if (list1.length <= 3) {
+      console.log('放量结果不足3只，不进行下一轮筛选')
+      list1.forEach((item, index) => {
+        console.log(`放量结果${index + 1}：`, item.code)
+      })
+      return
+    }
+    // 第二轮筛选
+    let list2 = []
+    for (let index = 0; index < list1.length; index++) {
+      const element = list1[index];
+      const code = element.code
+      const res = await getNetRealTime(code)
+      // console.log(`股票 ${code} 的实时资金流向:`, res)
+      if (res) {
+        if (res.extraLargeNetInflow > 0 && res.largeNetInflow > 0) {
+          list2.push(element)
+        }
+      }
+    }
+
+    list2.forEach((item, index) => {
+      console.log(`大单选股结果${index + 1}：`, item.code, item.name)
+    })
+
+  } catch (err) {
+    console.log('分析出错err=', err)
+  }
+}
+
+// const today = dayjs().format('20260313')
+const today = dayjs().format('YYYYMMDD')
+main(today, false).catch(console.error)
